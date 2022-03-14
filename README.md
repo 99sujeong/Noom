e398364f6de3dca05072a72d675cc578dcf112e0
99sujeong <ansdbs146@gmail.com>
2022-03-15 00:45:36 +0900

    WebSockets in NodeJS

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Noom
 
-Zoom Clong using NodeJS, WebRTC and Websockets.
+Zoom Clone using NodeJS, WebRTC and Websockets.
--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+import http from "http";
+import WebSocket from "ws";
 import express from "express";
 
 const app = express();
@@ -8,8 +10,15 @@ app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 
 app.use("/public", express.static(__dirname + "/public"));
-app.get("/", (req, res) => res.render("home"));
+app.get("/", (_, res) => res.render("home"));
+app.get("/*", (_, res) => res.redirect("/"));
 
 const handleListen = () => console.log('Listening on http://localhost:3000');
 
-app.listen(3000, handleListen);
+//  create a ws server on top of the http server. 
+// -> localhost can process both http and ws requests on the same port
+// == our server supports http protocol connections or ws protocol connection
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+server.listen(3000, handleListen);
