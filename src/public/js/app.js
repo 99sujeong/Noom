45be17d6033057f22e430d2eb66eb803f0fafbdb
99sujeong <ansdbs146@gmail.com>
2022-03-17 01:16:23 +0900

    Nickname part 1 - JSON obj -> String

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,15 +1,25 @@
 const messageList = document.querySelector("ul");
-const messageForm = document.querySelector("form");
+const nickForm = document.querySelector("#nick");
+const messageForm = document.querySelector("#message");
 // connect back-end from front-end
 // socket of app.js represents a connection to the server
 const socket = new WebSocket(`ws://${window.location.host}`);
+
+function makeMessage(type, payload) {
+    // make JSON obj(not text), before sending string
+    const msg = { type, payload };
+    // that obj creates string
+    return JSON.stringify(msg);
+}
 
 socket.addEventListener("open", () => {
     console.log("Connected to Server ");
 });
 
 socket.addEventListener("message", (message) => {
-    console.log("New message: ", message.data);
+    const li = document.createElement("li");
+    li.innerText = message.data;
+    messageList.append(li);
 });
 
 socket.addEventListener("close", () => {
@@ -19,9 +29,17 @@ socket.addEventListener("close", () => {
 function handleSubmit(event) {
     event.preventDefault();
     const input = messageForm.querySelector("input");
-    // sending something from a form in FE to BE
-    socket.send(input.value);
+    // every time that we send a message to our BE, we are sending a "string"
+    socket.send(makeMessage("new_message", input.value));
     input.value = "";
 }
 
+function handleNickSubmit(event) {
+    event.preventDefault();
+    const input = nickForm.querySelector("input");
+    // every time that we send a message to our BE, we are sending a "string"
+    socket.send(makeMessage("nickname", input.value));
+}
+
 messageForm.addEventListener("submit", handleSubmit);
+nickForm.addEventListener("submit", handleNickSubmit);
