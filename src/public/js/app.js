fd8ada10a6c0dedb2f4863df5d34003182b95c08
99sujeong <ansdbs146@gmail.com>
2022-03-17 21:09:40 +0900

    websocket ---> socket.io

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,2 +1,18 @@
 // connect socket.io with FE
 const socket = io();
+
+const welcome = document.getElementById("welcome");
+const form = welcome.querySelector("form");
+
+function handleRoomSubmit(event) {
+    event.preventDefault();
+    const input = form.querySelector("input");
+    // "emit" : send any event we make
+    // { } : send javascript object (don't have to send only string)
+    socket.emit("enter_room", { payload: input.value }, () => {
+        console.log("server is done!");
+    });
+    input.value = "";
+}
+
+form.addEventListener("submit", handleRoomSubmit);
